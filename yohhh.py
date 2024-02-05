8246c29211895e73576fd41ffbf23f858b515d92
DanielaOdhis <daniela.odhiambo@students.jkuat.ac.ke>
2024-02-05 16:46:44 +0300

    specifying data type of variable

--- yohhh.py
+++ yohhh.py
@@ -9,3 +9,8 @@ written in more than one line"""
 x=8;
 y="I am a girl"
 print("I am ",x," years and also",y)
+
+a = str(3)    # a will be '3'
+b = int(3)    # b will be 3
+c = float(3)  # c will be 3.0
+print(a,b,c)
