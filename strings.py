b6903e37ab76398f607cf6542088edbdd646d91e
DanielaOdhis <daniela.odhiambo@students.jkuat.ac.ke>
2024-02-11 21:27:31 +0300

    format strings

--- strings.py
+++ strings.py
@@ -49,3 +49,18 @@ a = "Hello"
 b = "World"
 c = a + " " + b
 print(c) #adds space between them
+
+#The format() method takes the passed arguments, formats them, and places them in the string where the placeholders {} are:
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+quantity = 3
+item_no = 567
+price = 49.95
+my_order = "I want {} pieces of item {} for {} dollars."
+print(my_order.format(quantity, item_no, price))
+
+#You can use index numbers {0} to be sure the arguments are placed in the correct placeholders:
+my_order = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(my_order.format(quantity, item_no, price))
